313dcb4e6cea0516ceaef4d32d37ea394c238aab
Gowni Gnanendra Reddy <140694819+gnana333@users.noreply.github.com>
2025-09-08 14:34:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -600,4 +600,5 @@ def get_image(image_id):
         return 'Error retrieving image', 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
